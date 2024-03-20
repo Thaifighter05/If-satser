24c672e88eba51d582e0b4d08a18ce1528d9904e
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2024-03-20 10:58:56 +0100

    2nd commit

--- If-satser/Program.cs
+++ If-satser/Program.cs
@@ -4,53 +4,69 @@ if (6>=3)
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
 
-Console.WriteLine("What's your name");
-string name = Console.ReadLine();
-name = name.ToLower();
 
 
-while(name != "noname")
+int x=1;
+while(x==1)
 {
-  Console.WriteLine("Wrong username");
-  Console.WriteLine("")
-  Console.ReadLine();
+  Console.WriteLine("What's your name");
+  string name = Console.ReadLine();
+  name = name.ToLower();
 
-  if (name == "noname")
+  if (name != "noname")
+  {
+  Console.WriteLine("Wrong username");
+  Console.ReadLine();
+  }
+
+  else if (name == "noname")
   {
     Console.WriteLine("correct");
     Console.ReadLine();
   }
-}
+      
 
-Console.WriteLine("what is your password");
+    
+  while(x==2)
+  {
+
+
+  Console.WriteLine("what is your password");
 string password = Console.ReadLine();
 password = password.ToLower();
 
-while ( password != "nopass")
-{
-    Console.WriteLine("What's your password");
+if (password != "nopass")
+  {
+
+    Console.WriteLine("Wrong password");
+    Console.WriteLine("what is your password");
+    password = password.ToLower();
     Console.ReadLine();
-    if(password == "nopass")
+  }
+   else if(password == "nopass")
     {
       Console.WriteLine("correct");
     }
 
+if( name == "noname" & password == "nopass") 
+{ 
+Console.WriteLine("Welcome");
+Console.ReadLine();
+
+
+  for (int i= 0; i <= 32; i++)
+  {
+   Console.WriteLine("Hello world");
+  }
 }
 
-// if( name == "noname" & password == "nopass") 
-// { 
-// Console.WriteLine("Welcome");
-// Console.ReadLine();
-// }
+  }
+    } 
 
 
 
 
-for (int i= 0; i <= 32; i++)
-{
-  Console.WriteLine("Hello world");
 
-}
 
 // while (i > 0)
 // {
